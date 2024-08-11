08384e702d3bb065a3bb6029c367e34bbe50e19d
Kateryna <kozlovets88@gmail.com>
2024-08-11 18:14:22 +0300

    add modal-order

---
+++ src/js/modal-order.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalOrderBtn: document.querySelector('[data-modal-order-open]'),
+    closeModalOrderBtn: document.querySelector('[data-modal-order-close]'),
+    modalOrder: document.querySelector('[data-modal-order]'),
+  };
+
+  refs.openModalOrderBtn.addEventListener('click', toggleModal);
+  refs.closeModalOrderBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modalOrder.classList.toggle('is-hidden');
+  }
+})();
