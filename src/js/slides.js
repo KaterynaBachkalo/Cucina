60653b212c5ccdd3b20b8729878b1d1818b2caac
Kateryna <kozlovets88@gmail.com>
2024-08-11 18:12:12 +0300

    add slides

---
+++ src/js/slides.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.horisontal-swiper-container', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 90,
+  // autoplay: {
+  // delay: 10000,
+  // disableOnInteraction: false,
+  // stopOnLastSlide: false,
+  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
